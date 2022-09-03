c8ee1592171d9d5ab7b5a3ae3f626641e9f3606d
Peter Wu <wizepeterw@peters-air-2.wireless.rit.edu>
2022-09-03 13:34:48 -0400

    git commit -m "folder cal "

---
+++ Clayhack project/src/routes/calendar/+page.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	//Document.getElementById("date").innerHTML = Date();
+</script>
+
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="content">
+	<h1>Current Classes</h1>
+	<p id="date">Date </p>
+
+	<ul>
+		<li style="display:inline">Sun</li>
+		<li style="display:inline">Mon</li>
+		<li style="display:inline">Tue</li>
+		<li style="display:inline">Wed</li>
+		<li style="display:inline">Thur</li>
+		<li style="display:inline">Sun</li>
+	</ul>
+
+	<pre>npm create svelte@latest</pre>
+
+	<p>
+		The page you're looking at is purely static HTML, with no client-side interactivity needed.
+		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
+		the devtools network panel and reloading.
+	</p>
+
+	<p>
+		The <a href="/todos">TODOs</a> page illustrates SvelteKit's data loading and form handling. Try using
+		it with JavaScript disabled!
+	</p>
+</div>
+
+<style>
+	.content {
+		width: 100%;
+		max-width: var(--column-width);
+		margin: var(--column-margin-top) auto 0 auto;
+	}
+
+	#date {
+		text-align: center;
+	}
+
+</style>
