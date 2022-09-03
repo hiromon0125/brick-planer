b559a3f617302127fcabb7ffd6ad2cc6f83e8483
Jamie Zheng <jamiezheng8653@gmail.com>
2022-09-03 14:21:09 -0400

    page tables

---
+++ Clayhack project/src/routes/activities catalogue/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	
+</script>
+
+<!-- <svelte:head>
+	<title>Activities Catalogue</title>
+	<meta name="description" content="About this app" />
+</svelte:head> -->
+
+<div class="content">
+	<!--Information pulled from inputs-->
+	<h1>Your Activities</h1>
+
+	<table>
+		<tr>
+			<td>A</td>
+		</tr>
+	</table>
+
+	<!--Information pulled from CampusGroups-->>
+	<h1>RIT Events</h1>
+
+	<table>
+		<tr>
+			<td>B</td>
+		</tr>
+	</table>
+
+</div>
+
+<style>
+	.content {
+		width: 100%;
+		max-width: var(--column-width);
+		margin: var(--column-margin-top) auto 0 auto;
+	}
+</style>
